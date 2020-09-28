a3fa6ead1bf6c1bb374761a5d5db8a2d86068e72
Devin Hunt <devin@hailpixel.com>
2020-09-28 16:20:25 +0100

    Modal now closes when clicked outside

--- src/layout/Modal.js
+++ src/layout/Modal.js
@@ -1,21 +1,36 @@
-const { style } = require("d3");
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import styles from '../hero.modules.css'
 
 import Title from '../components/Title'
 import Button from '../components/Button'
 
-const Modal = ({title = 'Modal', onClose, children}) => (
-  <div className={styles.Modal}>
-    <header>
-      <Title>{title}</Title> 
-      <Button onClick={() => onClose() }>Close</Button>
-    </header>
-    <div className={styles.ModalContent}>
-      {children}
+const Modal = ({title = 'Modal', onClose, children}) => {
+
+  const modalEl = useRef(null);
+
+  useEffect( () => {
+    const closeClickTest = event => {
+      if(event.target !== modalEl.current && !modalEl.current.contains(event.target)) {
+        onClose()
+      }
+    }
+
+    document.addEventListener('click', closeClickTest)
+    return () => document.removeEventListener('click', closeClickTest)
+  }, [])
+  
+  return (
+    <div className={ styles.Modal } ref={ modalEl }>
+      <header>
+        <Title>{title}</Title> 
+        <Button onClick={() => onClose() }>Close</Button>
+      </header>
+      <div className={styles.ModalContent}>
+        {children}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Modal
